acac7b1ac15b5cd690be5abb3b945b8c3d74537f
Petko D. Petkov <pdp@gnucitizen.org>
2018-10-30 15:48:13 +0000

    Better log groups.

--- modules/flow-vpc/main.tf
+++ modules/flow-vpc/main.tf
@@ -38,7 +38,10 @@ resource "aws_iam_role_policy" "main_policy" {
         "logs:CreateLogStream",
         "logs:PutLogEvents"
       ],
-      "Resource": "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${var.log_group_name}/*"
+      "Resource": [
+        "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${var.log_group_name}:*:*",
+        "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${var.log_group_name}"
+      ]
     }
   ]
 }

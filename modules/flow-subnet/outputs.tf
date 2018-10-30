c40f775fba397795edc437651dcc62370596b772
Petko D. Petkov <pdp@gnucitizen.org>
2018-10-30 16:52:36 +0000

    Output log group arn.

--- modules/flow-subnet/outputs.tf
+++ modules/flow-subnet/outputs.tf
@@ -2,6 +2,10 @@ output "log_group_name" {
   value = "${var.log_group_name}"
 }
 
+output "log_group_arn" {
+  value = "${var.aws_cloudwatch_log_group.arn}"
+}
+
 output "log_retention_in_days" {
   value = "${var.log_retention_in_days}"
 }
